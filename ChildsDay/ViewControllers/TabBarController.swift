4bc575ae4abd34175626411e9cc8daffeecf1de4
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2022-02-01 22:07:11 +0300

    Is initial commit

---
+++ ChildsDay/ViewControllers/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  ChildsDay
+//
+//  Created by Юрий Альт on 27.01.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
---
+++ ChildsDay/ViewControllers/TimerViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  ChildsDay
+//
+//  Created by Юрий Альт on 18.01.2022.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+//    let button: UIButton = {
+//        let button = UIButton(type: .system)
+//        button.backgroundColor = .green
+//        button.setTitle("Hello!", for: .normal)
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.layer.cornerRadius = 20
+//        return button
+//
+//    }()
+//
+//    let buttonTwo: UIButton = {
+//        let buttonTwo = UIButton()
+//        buttonTwo.backgroundColor = .red
+//        buttonTwo.translatesAutoresizingMaskIntoConstraints = false
+//        return buttonTwo
+//
+//    }()
+//
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.backgroundColor = .purple
+//
+//        view.addSubview(button)
+//        //view.addSubview(buttonTwo)
+//        setupButton()
+//        //setupButtonTwo()
+//    }
+//
+//
+//    func setupButton() {
+//        button.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+//        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
+//        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//    }
+//
+//    func setupButtonTwo() {
+//
+//    }
+    
+
+}
+
