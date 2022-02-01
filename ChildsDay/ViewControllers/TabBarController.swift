55012d172e950183366f2cd3dc5a569db9b21502
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2022-02-01 23:07:31 +0300

    Add UITabBarItems/ViewControllers

--- ChildsDay/ViewControllers/TabBarController.swift
+++ ChildsDay/ViewControllers/TabBarController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .gray
+        let timerViewController = TimerViewController()
+        let detailsViewController = DetailsViewController()
+        let adviceViewController = AdviceViewController()
+        
+        //Добавить картинки в Assets, избавиться от Force unwrap.
+        viewControllers = [
+            generateNavigationController(
+                rootViewController: timerViewController,
+                title: "Timer",
+                image: UIImage(systemName: "circle")!
+            ),
+            generateNavigationController(
+                rootViewController: detailsViewController,
+                title: "Details",
+                image: UIImage(systemName: "circle")!
+            ),
+            generateNavigationController(
+                rootViewController: adviceViewController,
+                title: "Advice",
+                image: UIImage(systemName: "circle")!
+            )
+        ]
+    }
+    
+    private func generateNavigationController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        return navigationVC
     }
 }
--- ChildsDay/ViewControllers/TimerViewController.swift
+++ ChildsDay/ViewControllers/TimerViewController.swift
@@ -9,48 +9,48 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
-//    let button: UIButton = {
-//        let button = UIButton(type: .system)
-//        button.backgroundColor = .green
-//        button.setTitle("Hello!", for: .normal)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.layer.cornerRadius = 20
-//        return button
-//
-//    }()
-//
-//    let buttonTwo: UIButton = {
-//        let buttonTwo = UIButton()
-//        buttonTwo.backgroundColor = .red
-//        buttonTwo.translatesAutoresizingMaskIntoConstraints = false
-//        return buttonTwo
-//
-//    }()
-//
-//
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        view.backgroundColor = .purple
-//
-//        view.addSubview(button)
-//        //view.addSubview(buttonTwo)
-//        setupButton()
-//        //setupButtonTwo()
-//    }
-//
-//
-//    func setupButton() {
-//        button.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-//        button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
-//        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
-//        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-//    }
-//
-//    func setupButtonTwo() {
-//
-//    }
-    
+    let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .green
+        button.setTitle("Hello!", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 20
+        return button
+    }()
+
+    let buttonTwo: UIButton = {
+        let buttonTwo = UIButton(type: .system)
+        buttonTwo.backgroundColor = .red
+        buttonTwo.setTitle("Baby", for: .normal)
+        buttonTwo.translatesAutoresizingMaskIntoConstraints = false
+        buttonTwo.layer.cornerRadius = 20
+        return buttonTwo
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        view.addSubview(button)
+        view.addSubview(buttonTwo)
+        setupButton()
+        setupButtonTwo()
+    }
+
+
+    func setupButton() {
+        button.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+    func setupButtonTwo() {
+        buttonTwo.leftAnchor.constraint(equalTo: view.rightAnchor, constant: -200).isActive = true
+        buttonTwo.topAnchor.constraint(equalTo: button.topAnchor).isActive = true
+        buttonTwo.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        buttonTwo.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+
+    }
 
 }
 
